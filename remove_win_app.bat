56f6917bc6c94eaba32e7e85ddb5fa768b5afb50
Taehun Kim <taehun.kim@sportngin.com>
2018-01-13 00:53:21 -0600

    Create remove_win_app.bat

---
+++ remove_win_app.bat
@@ -0,0 +1,13 @@
+PowerShell.exe -Command "Get-AppxPackage *3dbuilder* | Remove-AppxPackage"
+PowerShell.exe -Command "Get-AppxPackage *officehub* | Remove-AppxPackage"
+PowerShell.exe -Command "Get-AppxPackage *skypeapp* | Remove-AppxPackage"
+PowerShell.exe -Command "Get-AppxPackage *getstarted* | Remove-AppxPackage"
+PowerShell.exe -Command "Get-AppxPackage *zunemusic* | Remove-AppxPackage"
+PowerShell.exe -Command "Get-AppxPackage *windowsmaps* | Remove-AppxPackage"
+PowerShell.exe -Command "Get-AppxPackage *bingfinance* | Remove-AppxPackage"
+PowerShell.exe -Command "Get-AppxPackage *bingfinance* | Remove-AppxPackage"
+PowerShell.exe -Command "Get-AppxPackage *windowsstore* | Remove-AppxPackage"
+PowerShell.exe -Command "Get-AppxPackage *bingweather* | Remove-AppxPackage"
+PowerShell.exe -Command "Get-AppxPackage *xboxapp* | Remove-AppxPackage"
+
+pause
