8e3bf601d7df6b9e0e73fc27e4ca28fceadfb9d3
Taehun Kim <taehun.kim@sportngin.com>
2018-03-15 01:05:07 -0700

    11.x above only

---
+++ claymore_dual_miner_scripts/ETH_XSH_DUAL.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+EthDcrMiner64.exe -epool eth-us-west1.nanopool.org:9999 -ewal 0x9dBfD51f29dCa14E7AEA165cDC4b4EE7965555Fe/miner999/youremail@gmail.com -allpools 1 -allcoins exp -gser 2 -logfile lastlog_noappend.txt -r 1 -dpool stratum+tcp://s.antminepool.com:5766 -dwal SQm3AFMrJ1iPKmSqpCbmEeuH5oBDadwjhb -dcoin blake2s -dpsw c=XSHB.1.999 -mport 3333 -r 1 -mport 3333 -dcri 44
